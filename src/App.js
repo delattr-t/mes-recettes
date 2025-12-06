ec69bacc354970aa3a28d872dcf4ed5d2bf097ea
delattr-t <thibaut.delattre@protonmail.com>
2025-12-06 23:30:32 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,9 @@
+const FIREBASE_CONFIG = {
+  apiKey: "VOTRE_VRAIE_API_KEY",  // ← Remplacez par vos vraies valeurs
+  authDomain: "votre-projet.firebaseapp.com",
+  projectId: "votre-projet-id",
+  storageBucket: "votre-projet.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:xxxxx",
+  databaseURL: "https://votre-projet-default-rtdb.firebaseio.com"
+};
