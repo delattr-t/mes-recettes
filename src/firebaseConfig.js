8eb8a2ea362e965cc0a2ea0efb2a141e50ce11d8
delattr-t <thibaut.delattre@protonmail.com>
2025-12-07 17:03:05 +0100

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDyhJKh9-nElxZ6bpK5EFk7r2wy0oy8u1M",
@@ -14,5 +15,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
-export { database };
+export { database, auth, googleProvider };
