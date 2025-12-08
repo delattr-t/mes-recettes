cabd4ccd170027de31c659ec473aa1f3a18bdc9b
delattr-t <thibaut.delattre@protonmail.com>
2025-12-08 23:54:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -329,7 +329,7 @@ export default function RecipeManager() {
                 </p>
               </div>
             ) : (
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {filteredRecipes.map((recipe) => (
                   <div 
                     key={recipe.id} 
